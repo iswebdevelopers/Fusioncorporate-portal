4b8ee47eecbf1097c890eb321dbcdd3fcd8dff16
Ram Gopinath <RGopinath@coloradogroup.com.au>
2017-12-13 18:50:22 +1100

    more template fix

--- portal/resources/views/labels/templates/sticky.blade.php
+++ portal/resources/views/labels/templates/sticky.blade.php
@@ -10,40 +10,40 @@
 		$margin_top = ceil(($height/100) * 10);
 
 		//fontsizes
-		$font_1 = ceil(($height/100) * 20);
-		$font_2 = ceil(($height/100) * 10);
+		$font_1 = ceil(($height/100) * 15);
+		$font_2 = ceil(($height/100) * 7);
 
 		//count
 		$count = 1;
 	?>
 	@if(!empty($data)) 		
 		@foreach ($data as $item)
-			@if($count == 0)
+			@if($count == 1)
 				^XA
 			@endif
 			@for ($i=1; $i <= $item['quantity']; $i++)
 					^FX Top section.
 					^CF0,{{$font_1}}
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top}}^FD{{$item['stockroomlocator']}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top}}^FD{{$item['stockroomlocator']}}^FS
 
 					^FX Second.
 					^CF0,{{$font_2}}
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top * 2.5}}^FDSize:{{$item['size']}}^FS
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top * 3.3}}^FDitem: {{$item['item']}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top * 2.5}}^FDSize:{{$item['size']}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top * 3.3}}^FDitem: {{$item['item']}}^FS
 
 					^FX Third section with barcode.
 					^BY2,2,150
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top * 4}}^BE^FD{{$item['barcode']}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top * 4}}^BE^FD{{$item['barcode']}}^FS
 
 					^FX Fourth section (the two boxes on the bottom).
 					^CF0,{{$font_2}},
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top * 7}}^FH^FD{{str_replace('~','_7e',$item['description'])}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top * 7}}^FH^FD{{str_replace('~','_7e',$item['description'])}}^FS
 					^CF0,{{$font_2}},
-					^FO{{$margin_left + ($count * $width)}},{{$margin_top * 8}}^FH^FD{{str_replace('~','_7e',$item['colour'])}}^FS
+					^FO{{$margin_left + (($count - 1) * $width)}},{{$margin_top * 8}}^FH^FD{{str_replace('~','_7e',$item['colour'])}}^FS
 				
-				@if($count < $label_per_row)
+				@if(($count < $label_per_row) and ($count < $item['quantity']))
 					<?php $count++; ?>
-				@elseif ($count >= $label_per_row) 
+				@elseif (($count >= $label_per_row) || ($count <= $item['quantity'])) 
 					<?php $count = 1; ?>
 				^XZ
 				<br/>

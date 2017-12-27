3a7b49a9c6e0aac506629741da8c388aeaf292e7
Ram Gopinath <RGopinath@coloradogroup.com.au>
2017-12-27 17:22:31 +1100

    template fix and carton tab fix

--- portal/resources/views/labels/templates/mixedcarton.blade.php
+++ portal/resources/views/labels/templates/mixedcarton.blade.php
@@ -1,13 +1,10 @@
 <!-- mixed carton print -->
-	@for ($i = 1; $i <= $mixed; $i++)
-		^XA
-				^FX Top section.
-				^CF0,100
-				^FO90,300^FD************^FS
-				^FO90,400^FDMixed^FS
-				^FO90,500^FDOr^FS
-				^FO90,600^FH^FDIncomplete^FS
-				^FO90,700^FD************^FS
-			^XZ
-			<br/>
-	@endfor
+^XA
+	^FX Top section.
+	^CF0,100
+	^FO90,300^FD************^FS
+	^FO90,400^FDMixed^FS
+	^FO90,500^FDOr^FS
+	^FO90,600^FH^FDIncomplete^FS
+	^FO90,700^FD************^FS
+^XZ

a29a7481e6fa540da9e38e3cf088271104e75d3c
Ram Gopinath <RGopinath@coloradogroup.com.au>
2017-12-29 15:56:10 +1100

    mixed carton implementation

---
+++ portal/resources/views/labels/mixed_tab.blade.php
@@ -0,0 +1,26 @@
+<div class="tab-pane fade" id="mixed">
+	<h4>Carton Mixed</h4>
+	<div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>Order No.</th>
+                    <th>Type</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+            <form action="{{ action('LabelController@printmixed') }}" method="post">
+            <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+            <input name="type" class="form-control" type="hidden" value="mixed">
+                <tr>
+                <input name="order_no" class="form-control" type="hidden" value="{{$order_no}}">
+                    <td>{{$order_no}}</td>
+                    <td>Mixed Carton</td>
+                    <td class="col-xs-1"><input name="quantity" class="form-control" type="text" value="4"></td>
+                </tr>
+            </tbody>
+        </table><button type="submit" class="btn btn-primary">Generate</button>
+        </div>
+    </form>
+</div>    

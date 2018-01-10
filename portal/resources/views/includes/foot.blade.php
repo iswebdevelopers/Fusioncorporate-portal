9e9bc611ba68fa9c5ca88d0439d186ab5592f9c2
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-01-10 14:54:18 +1100

    footer enhancement - year to be detected by blade

--- portal/resources/views/includes/foot.blade.php
+++ portal/resources/views/includes/foot.blade.php
@@ -1,4 +1,4 @@
 <footer>
 	<img class="center-block" style="margin: 0 auto;" src="{{asset('img/logo.jpg')}}" alt="Fusion Retail Brands"/>
-	<p class="text-center">All right reserved. &copy; - Fusion Retail Brands 2018. Template by: <a target="_blank" href="https://webthemez.com">WebThemez</a></p>
+	<p class="text-center">All right reserved. &copy; - Fusion Retail Brands {{ date('Y') }}. Template by: <a target="_blank" href="https://webthemez.com">WebThemez</a></p>
 </footer>

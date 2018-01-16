4065a76109f1e1b26c176abbc4aa3559ac44ad8d
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-01-16 16:06:00 +1100

    change of button class to btn-sm, tab to justified and supplier tab to reflect changes in admin tab

--- portal/resources/views/labels/supplier_carton_tab.blade.php
+++ portal/resources/views/labels/supplier_carton_tab.blade.php
@@ -1,69 +1,70 @@
 <div class="tab-pane fade active in" id="carton">
-	<div class="table-responsive">
+	@if(!empty($orderdetails['cartonpack']))
+        <div class="table-responsive">
+            <h4>Carton Pack - {{$order_no}}</h4>
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Style</th>
+                        <th>Item Number</th>
+                        <th>Description</th>
+                        <th>Pack Quantity</th>
+                        <th>Type</th>
+                        <th>Quantity</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($orderdetails['cartonpack'] as $order)
+                    <tr>
+                        <td>{{$order['style']}}</td>
+                        <td>{{$order['item']}}</td>
+                        <td>{{$order['description']}}</td>
+                        <td class="col-xs-1">{{$order['quantity']}}</td>
+                        <td class="col-xs-1">{{$order['pick_location'] or '6'}}</td>
+                        <td class="col-xs-1">Pack - {{$order['pack_type']}}</td>
+                        <td class="col-xs-1"><a class="btn btn-primary btn-sm" href="/portal/label/print/{{strtolower($order['carton_type'])}}/{{$order['order_number']}}/{{$order['item']}}">Generate</a></td>
+                    </tr>
+                @endforeach    
+                </tbody>
+            </table>
+        </div>
+    @endif
+
+    @if(!empty($orderdetails['cartonloose']))
+        <div class="table-responsive">
+            <h4>Carton Loose</h4>
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Style</th>
+                        <th>Item Number</th>
+                        <th>Description</th>
+                        <th>Size</th>
+                        <th>Type</th>
+                        <th>Quantity</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($orderdetails['cartonloose'] as $order)
+                    <tr>
+                        <td>{{$order['style']}}</td>
+                        <td>{{$order['item']}}</td>
+                        <td>{{$order['description']}}</td>
+                        <td class="col-xs-1">{{$order['item_size']}}</td>
+                        <td class="col-xs-1">Loose</td>
+                        <td class="col-xs-1">{{$order['quantity']}}</td>
+                        <td class="col-xs-1"><a class="btn btn-primary btn-sm" href="/portal/label/print/{{strtolower($order['carton_type'])}}/{{$order['order_number']}}/{{$order['item']}}">Generate</a></td>
+                    </tr>
+                @endforeach    
+                </tbody>
+            </table>
+        </div>
+    @endif
     <p class="pull-right">
     @if((!empty($orderdetails['cartonpack'])) or (!empty($orderdetails['cartonloose'])))
-        <a class="btn btn-primary btn-md" href="/portal/label/print/cartons/{{$order_no}}">Generate all Carton</a>
+        <a class="btn btn-primary btn-sm" href="/portal/label/print/cartons/{{$order_no}}">Generate all Carton</a>
     @endif
     </p>
-    @if(!empty($orderdetails['cartonpack']))
-        <h4>Carton Pack</h4>
-        <table class="table table-striped table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th>Order No.</th>
-                    <th>Style</th>
-                    <th>Item Number</th>
-                    <th>Pack Quantity</th>
-                    <th>Type</th>
-                    <th>Quantity</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($orderdetails['cartonpack'] as $order)
-                <tr>
-                    <td>{{$order['order_number']}}</td>
-                    <td>{{$order['style']}}</td>
-                    <td>{{$order['item']}}</td>
-                    <td>{{$order['quantity']}}</td>
-                    <td>{{$order['pick_location'] or '6'}}</td>
-                    <td>Pack - {{$order['pack_type']}}</td>
-                    <td><a class="btn btn-primary btn-sm" href="/portal/label/print/{{strtolower($order['carton_type'])}}/{{$order['order_number']}}/{{$order['item']}}">Generate</a></td>
-                </tr>
-            @endforeach    
-            </tbody>
-        </table>
-        @endif
-    </div>
-    @if(!empty($orderdetails['cartonloose']))
-        <h4>Carton Loose</h4>
-        <div class="table-responsive">
-        <table class="table table-striped table-bordered table-hover">
-            <thead>
-                <tr>
-                    <th>Order No.</th>
-                    <th>Style</th>
-                    <th>Item Number</th>
-                    <th>Size</th>
-                    <th>Type</th>
-                    <th>Quantity</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach ($orderdetails['cartonloose'] as $order)
-                <tr>
-                    <td>{{$order['order_number']}}</td>
-                    <td>{{$order['style']}}</td>
-                    <td>{{$order['item']}}</td>
-                    <td>{{$order['item_size']}}</td>
-                    <td>Loose</td>
-                    <td>{{$order['quantity']}}</td>
-                    <td><a class="btn btn-primary btn-sm" href="/portal/label/print/{{strtolower($order['carton_type'])}}/{{$order['order_number']}}/{{$order['item']}}">Generate</a></td>
-                </tr>
-            @endforeach    
-            </tbody>
-        </table>
-    </div>
-    @endif
 </div>

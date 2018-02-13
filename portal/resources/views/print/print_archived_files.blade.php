2e2862435f60e40df5b4847e603d0fc4a0d724ed
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-02-13 16:12:04 +1100

    changes to show description on print shop

--- portal/resources/views/print/print_archived_files.blade.php
+++ portal/resources/views/print/print_archived_files.blade.php
@@ -7,6 +7,7 @@
                     <th>Order No.</th>
                     <th>Archived Date</th>
                     <th>Label Type</th>
+                    <th>Description</th>
                     <th>Quantity</th>
                     <th>Action</th>
                 </tr>
@@ -17,6 +18,7 @@
                         <td><a href="/portal/label/order/{{$archive->order_id}}">{{$archive->order_id}}</a></td>
                         <td>{{$archive->updated_at}}</td>
                         <td>{{$archive->type}}</td>
+                        <td>{{$archive->description}}</td>
                         <td>{{$archive->quantity}}</td>
                         <td>
                             <a class="btn btn-default btn-sm" onclick="printZPL({{$archive->id}});">Print</a>

387dcf06464a4d12483cdc6d1847805b8501ef1c
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-02-21 11:39:59 +1100

    Laravel mix - minification and compilation of js and css

--- portal/resources/views/includes/footer.blade.php
+++ portal/resources/views/includes/footer.blade.php
@@ -2,5 +2,4 @@
 <script src="{{ asset('js/jquery/jquery.metisMenu.js') }}"></script>
 
 <!-- Custom Js -->
-<script src="{{ asset('js/custom/scripts.js') }}"></script>
 <script src="{{ asset('js/custom/main.js') }}"></script>
--- portal/webpack.mix.js
+++ portal/webpack.mix.js
@@ -11,5 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+//mix.js('resources/assets/js/app.js', 'public/js')
+mix.js(['resources/assets/js/main.js','resources/assets/js/scripts.js'],'public/js/custom/main.js')
+	.styles(['resources/assets/css/app.css','resources/assets/css/custom-styles.css'], 'public/css/main.min.css')
+	.styles('resources/assets/css/venobox.css', 'public/css/venobox.min.css');

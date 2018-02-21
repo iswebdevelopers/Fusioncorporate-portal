d014b645dc326ae4eafcaff23f002886aadd4bea
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-02-21 16:03:31 +1100

    FAQ chnags to add screenshots

--- portal/public/js/custom/main.js
+++ portal/public/js/custom/main.js
@@ -1,1 +1,305 @@
-!function(e){var n={};function t(i){if(n[i])return n[i].exports;var a=n[i]={i:i,l:!1,exports:{}};return e[i].call(a.exports,a,a.exports,t),a.l=!0,a.exports}t.m=e,t.c=n,t.d=function(e,n,i){t.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:i})},t.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(n,"a",n),n},t.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},t.p="",t(t.s=0)}([function(e,n,t){t(1),e.exports=t(2)},function(e,n){!function(e){"use strict";var n={initFunction:function(){e("#main-menu").metisMenu(),e(window).bind("load resize",function(){e(this).width()<768?e("div.sidebar-collapse").addClass("collapse"):e("div.sidebar-collapse").removeClass("collapse")})},initialization:function(){n.initFunction()}};e(document).ready(function(){n.initFunction()})}(jQuery)},function(e,n){function t(e,n){e.each(function(){$(this).prop("checked",n),$(this).trigger("change")})}$(function(){(function(){var e=window.navigator.userAgent,n=e.indexOf("MSIE ");if(n>0)return parseInt(e.substring(n+5,e.indexOf(".",n)),10);if(e.indexOf("Trident/")>0){var t=e.indexOf("rv:");return parseInt(e.substring(t+3,e.indexOf(".",t)),10)}var i=e.indexOf("Edge/");if(i>0)return parseInt(e.substring(i+5,e.indexOf(".",i)),10);return!1})()&&$("#browser-warning").parent().show(),$("select#role").selectmenu({change:function(e,n){$("div","div#extra-fields").hide();var t=n.item.value;$("div#"+t).show(),$("div#"+t+" input[name=role_id]").prop("disabled",!1)}});var e=$("p.alert-info").parent();setTimeout(function(){e.hide(1e3)},1e4);var n=$("#token").val();$("#supplier_autocomplete").autocomplete({source:function(e,t){$.ajax({url:"/portal/supplier/search/"+e.term+"?token="+n,statusCode:{401:function(){window.location.replace("/portal/login")}},success:function(e){t($.map(e.data,function(e){var n=new Object;return n.label=e.name,n.value=e.id,n}))},error:function(e,n,t){alert("Something has gone wrong. Please refresh.")}})},minLength:3,select:function(e,n){$("#extra-fields input[name=role_id]").val(n.item.id)}}),$("div","div#extra-fields").hide(),$("#extra-fields input[name=role_id]").prop("disabled",!0),$("select#role").change(function(){$("div","div#extra-fields").hide();var e=$(this).val();$("div#"+e).show(),$("div#"+e+" input[name=role_id]").prop("disabled",!1)}),$("#carton,#unit").on("change","input[type='checkbox']",function(){var e,n,t=$(this).data("item"),i=$("input."+t);e=i,n=this.checked,e.each(function(){$(this).prop("disabled",!n)})}),$("table").on("click","a.selectall",function(e){e.preventDefault();var n=$(this).parent().data("type");t($("input."+n),!0)}),$("table").on("click","a.selectnone",function(e){e.preventDefault();var n=$(this).parent().data("type");t($("input."+n),!1)}),$(".venbobox").venobox({framewidth:"500px",frameheight:"auto",border:"10px",titleattr:"Password Recovery"}),$("#users-list, #suppliers-list").DataTable({sPaginationType:"full_numbers",bPaginate:!0,iDisplayLength:10,searching:!0,ordering:!1}),$("#cartonfiles").DataTable({sPaginationType:"full_numbers",bPaginate:!0,iDisplayLength:10,searching:!1,ordering:!1}),$("#stickyfiles").DataTable({sPaginationType:"full_numbers",bPaginate:!0,iDisplayLength:10,searching:!1,ordering:!1})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+__webpack_require__(1);
+module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+/*------------------------------------------------------
+    Author : www.webthemez.com
+    License: Commons Attribution 3.0
+    http://creativecommons.org/licenses/by/3.0/
+---------------------------------------------------------  */
+
+(function ($) {
+    "use strict";
+
+    var mainApp = {
+
+        initFunction: function initFunction() {
+            /*MENU 
+            ------------------------------------*/
+            $('#main-menu').metisMenu();
+
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse');
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse');
+                }
+            });
+        },
+
+        initialization: function initialization() {
+            mainApp.initFunction();
+        }
+        // Initializing ///
+
+    };$(document).ready(function () {
+        mainApp.initFunction();
+    });
+})(jQuery);
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+$(function () {
+    //detect and show message to use other browser 
+    var IE = detectIE();
+
+    if (IE) {
+        $("#browser-warning").parent().show();
+    }
+
+    //user creation form- role change based on role selection functionality  
+    $("select#role").selectmenu({
+        change: function change(event, data) {
+            // hide previously shown in target div
+            $("div", "div#extra-fields").hide();
+
+            // read id from your select
+            var value = data.item.value;
+            // show element with selected id
+            $("div#" + value).show();
+            $("div#" + value + " input[name=role_id]").prop("disabled", false);
+        }
+    });
+
+    //Hide all alert after 10 seconds
+    var infoWindow = $("p.alert-info").parent();
+    setTimeout(function () {
+        infoWindow.hide(1000);
+    }, 10000);
+
+    //supplier role to have id of the supplier functionality autocomplete
+    var token = $("#token").val();
+    $("#supplier_autocomplete").autocomplete({
+        source: function source(request, response) {
+            $.ajax({
+                url: "/portal/supplier/search/" + request.term + "?token=" + token,
+                statusCode: {
+                    401: function _() {
+                        window.location.replace("/portal/login");
+                    }
+                },
+                success: function success(data) {
+                    response($.map(data.data, function (item) {
+                        var AC = new Object();
+
+                        //autocomplete default values REQUIRED
+                        AC.label = item.name;
+                        AC.value = item.id;
+
+                        return AC;
+                    }));
+                },
+                error: function error(XMLHttpRequest, textStatus, errorThrown) {
+                    alert("Something has gone wrong. Please refresh.");
+                }
+            });
+        },
+        minLength: 3,
+        select: function select(event, ui) {
+            $("#extra-fields input[name=role_id]").val(ui.item.id);
+        }
+    });
+
+    //user creation form- role change based on role selection functionality
+    $("div", "div#extra-fields").hide();
+    $("#extra-fields input[name=role_id]").prop("disabled", true);
+
+    $("select#role").change(function () {
+        // hide previously shown in target div
+        $("div", "div#extra-fields").hide();
+
+        // read id from your select
+        var value = $(this).val();
+        // show element with selected id
+        $("div#" + value).show();
+        $("div#" + value + " input[name=role_id]").prop("disabled", false);
+    });
+
+    //on load check and disable/enable selection
+    // $("input.pack,input.loose,input.sticky").each(function(){
+    //     var elclass = $(this).data("item");
+    //     var elements = $("input."+elclass);
+    //     changeInputState(elements,this.checked);
+    // });
+
+    //selection of carton labels and sticky labels to generate labels
+    $("#carton,#unit").on("change", "input[type='checkbox']", function () {
+        var elclass = $(this).data("item");
+        var elements = $("input." + elclass);
+        changeInputState(elements, this.checked);
+    });
+
+    //selection of carton labels and sticky labels to generate labels
+    $("table").on("click", "a.selectall", function (e) {
+        e.preventDefault();
+        var elclass = $(this).parent().data("type");
+        var elements = $("input." + elclass);
+        changeSelectCheckboxState(elements, true);
+    });
+
+    //selection of carton labels and sticky labels to generate labels
+    $("table").on("click", "a.selectnone", function (e) {
+        e.preventDefault();
+        var elclass = $(this).parent().data("type");
+        var elements = $("input." + elclass);
+        changeSelectCheckboxState(elements, false);
+    });
+
+    //fancybox like pop up for account password recovery
+    $(".venbobox").venobox({
+        framewidth: "500px",
+        frameheight: "auto",
+        border: "10px",
+        titleattr: "Password Recovery"
+    });
+
+    $("a.img-gallery").venobox({
+        titleattr: 'data-title'
+    });
+
+    //Datatables to have pagination
+    $("#users-list, #suppliers-list").DataTable({
+        "sPaginationType": "full_numbers",
+        "bPaginate": true,
+        "iDisplayLength": 10,
+        "searching": true,
+        "ordering": false
+    });
+
+    $("#cartonfiles").DataTable({
+        "sPaginationType": "full_numbers",
+        "bPaginate": true,
+        "iDisplayLength": 10,
+        "searching": false,
+        "ordering": false
+    });
+
+    $("#stickyfiles").DataTable({
+        "sPaginationType": "full_numbers",
+        "bPaginate": true,
+        "iDisplayLength": 10,
+        "searching": false,
+        "ordering": false
+    });
+});
+
+function changeInputState(inputs, state) {
+    inputs.each(function () {
+        $(this).prop("disabled", !state);
+    });
+}
+
+function changeSelectCheckboxState(checkboxs, state) {
+    checkboxs.each(function () {
+        $(this).prop("checked", state);
+        $(this).trigger("change");
+    });
+}
+
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+        // Edge (IE 12+) => return version number
+        return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+/***/ })
+/******/ ]);

e1d5f37d941697f97cd2be2536b558b8fae6030d
Ram Gopinath <RGopinath@coloradogroup.com.au>
2018-03-01 11:25:45 +1100

    unit template fix item label template for single unit printing

--- portal/resources/views/labels/templates/unit/item.blade.php
+++ portal/resources/views/labels/templates/unit/item.blade.php
@@ -33,24 +33,28 @@
 		<?php $total = $sticky['quantity'];?>
 
 		@for ($i=1; $i <= $sticky['quantity']; $i++)
-			@if($label_no == 1)
+			@if($i == 1)
 				^XA
 			@endif	
 			^FX Top section.
 			^CF0,{{$font_1}}
 			^FO{{$margin_left + (($label_no - 1) * $width)}},{{$margin_top}}^FD{{$sticky['item']}}^FS
+
 			^FX Second.
 			^CF0,{{$font_1}}
 			^FO{{$margin_left + (($label_no - 1) * $width)}},{{$margin_top * 2}}^FDSize:{{$sticky['item_size']}}^FS
+
 			^FX Third section with barcode.
 			{{$barcode}}
 			^FO{{$margin_left + (($label_no - 1) * $width)}},{{$margin_top * 3.5}}
 			{{ config('ticket.barcodetype.'.$sticky['barcode_type']) }}^FD{{$sticky['barcode']}}^FS
+
 			^FX Fourth section (the two boxes on the bottom).
 			^CF0,{{$font_2}},
 			^FO{{$margin_left + (($label_no - 1) * $width)}},{{$margin_top * 7}}^FH^FD{{str_replace('~','_7e',$sticky['description'])}}^FS
 			^CF0,{{$font_2}},
 			^FO{{$margin_left + (($label_no - 1) * $width)}},{{$margin_top * 8}}^FH^FD{{str_replace('~','_7e',$sticky['colour'])}}^FS
+		
 			@if ($count == $total)
 				^XZ
 			@elseif ($count % $label_per_row == 0)
@@ -59,7 +63,7 @@
 				<?php $label_no = 1; ?>
 			@endif
 
-			@if($count < $total)
+			@if(($count < $total) and ($label_per_row > 1))
 				<?php $label_no++; ?>
 				<?php $count++; ?>
 			@endif	
